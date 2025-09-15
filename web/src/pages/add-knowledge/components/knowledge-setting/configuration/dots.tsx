dd6771ed9802b21babab1a5ab71dda223c89591d
weizxfree <weizhangxiang2022@gmail.com>
2025-09-15 11:25:32 +0800

    1. fix 创建知识库时 dots 前端配置项缺失问题

--- web/src/pages/add-knowledge/components/knowledge-setting/configuration/dots.tsx
+++ web/src/pages/add-knowledge/components/knowledge-setting/configuration/dots.tsx
@@ -1,7 +1,20 @@
+import {
+  AutoKeywordsItem,
+  AutoQuestionsItem,
+} from '@/components/auto-keywords-item';
 import ChunkingConfig from '@/components/chunking-config';
 import { DatasetConfigurationContainer } from '@/components/dataset-configuration-container';
+import Delimiter from '@/components/delimiter';
+import ExcelToHtml from '@/components/excel-to-html';
+import LayoutRecognize from '@/components/layout-recognize';
+import MaxTokenNumber from '@/components/max-token-number';
+import PageRank from '@/components/page-rank';
+import ParseConfiguration from '@/components/parse-configuration';
+import GraphRagItems from '@/components/parse-configuration/graph-rag-items';
 import { useTranslate } from '@/hooks/common-hooks';
-import { Form, Input } from 'antd';
+import { Divider, Form, Input } from 'antd';
+import { TagItems } from '../tag-item';
+import { ChunkMethodItem, EmbeddingModelItem } from './common-item';
 
 export function DOTSConfiguration() {
   const { t } = useTranslate('knowledgeConfiguration');
@@ -17,10 +30,40 @@ export function DOTSConfiguration() {
         <Input />
       </Form.Item>
 
+      <DatasetConfigurationContainer>
+        <LayoutRecognize></LayoutRecognize>
+        <EmbeddingModelItem></EmbeddingModelItem>
+        <ChunkMethodItem></ChunkMethodItem>
+        <MaxTokenNumber></MaxTokenNumber>
+        <Delimiter></Delimiter>
+      </DatasetConfigurationContainer>
+
+      <Divider></Divider>
+
       {/* DOTS 分块策略配置 */}
       <DatasetConfigurationContainer>
         <ChunkingConfig />
       </DatasetConfigurationContainer>
+
+      <Divider></Divider>
+
+      <DatasetConfigurationContainer>
+        <PageRank></PageRank>
+        <AutoKeywordsItem></AutoKeywordsItem>
+        <AutoQuestionsItem></AutoQuestionsItem>
+        <ExcelToHtml></ExcelToHtml>
+        <TagItems></TagItems>
+      </DatasetConfigurationContainer>
+
+      <Divider></Divider>
+
+      <DatasetConfigurationContainer>
+        <ParseConfiguration></ParseConfiguration>
+      </DatasetConfigurationContainer>
+
+      <Divider></Divider>
+
+      <GraphRagItems></GraphRagItems>
     </section>
   );
 }
